6db415ac72bebc34049e8ed276a88a3b6b4ae842
Kunal Jindal <kunaljindal@mnnit.ac.in>
2023-09-20 22:36:21 +0530

    Add Customer APIs

---
+++ src/main/java/com/example/Swiggato/dto/response/CartResponse.java
@@ -0,0 +1,18 @@
+package com.example.Swiggato.dto.response;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.util.List;
+
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CartResponse {
+
+    int cartTotal;
+
+    List<FoodResponse> foodItems;
+}
